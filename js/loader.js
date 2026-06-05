91d3d80cc829fc121e354288501b2d22a1061017
udan-insith <udaninsith23613@gmail.com>
2026-06-05 14:26:36 +0530

    starting fresh again beacause some mistakes happened ;-(

--- js/loader.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader");
-  loader.style.opacity = "0";
-
-  setTimeout(() => {
-    loader.style.display = "none";
-  }, 500);
-});
--- js/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".hamburger-menu");
-const navLinks = document.querySelector(".nav-links");
-
-menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
